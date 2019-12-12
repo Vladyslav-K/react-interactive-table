423c70ec391a977437303c8f088f14b62942a717
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2019-12-12 18:08:47 +0200

    Updates

---
+++ precache-manifest.2d9eec116fa3b05c87d432a0a06af4a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aec285e4d606b43ab00afd8ac785fb7",
+    "url": "/react-interactive-table/index.html"
+  },
+  {
+    "revision": "b750b00864bc1eb13eab",
+    "url": "/react-interactive-table/static/js/2.5c1bd94e.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/react-interactive-table/static/js/2.5c1bd94e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4046e6fa0fac011d61b9",
+    "url": "/react-interactive-table/static/js/main.b331bd40.chunk.js"
+  },
+  {
+    "revision": "92ae3e59c880fe99c787",
+    "url": "/react-interactive-table/static/js/runtime-main.a2de3565.js"
+  }
+]);
