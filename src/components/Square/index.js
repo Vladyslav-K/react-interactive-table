78b71c20fe46f2bebd53c09ca7d036eeb3e183c3
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2019-12-18 14:20:31 +0200

    refactor(state): change the interaction of state and components

--- src/components/Square/index.js
+++ src/components/Square/index.js
@@ -2,22 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const Wrapper = styled.div`
-  height: 100vh;
-  width: 100vw;
-
-  margin: 0;
-  padding: 0;
-
-  user-select: none;
-`;
-
 const Container = styled.div`
   position: absolute;
 
-  left: ${props => props.containerPosition.left};
-  top: ${props => props.containerPosition.top};
-
+  margin: 100px;
+  
   user-select: none;
 `;
 
@@ -28,8 +17,8 @@ const Table = styled.table`
 `;
 
 const Cell = styled.td`
-  height: ${props => props.cellSize}px;
-  width: ${props => props.cellSize}px;
+  height: ${({ cellSize }) => cellSize}px;
+  width: ${({ cellSize }) => cellSize}px;
 
   padding: 0;
 
@@ -44,13 +33,13 @@ const Button = styled.div`
   align-items: center;
   position: absolute;
 
-  height: ${props => props.cellSize}px;
-  width: ${props => props.cellSize}px;
+  height: ${({ cellSize }) => cellSize}px;
+  width: ${({ cellSize }) => cellSize}px;
   margin: 2px;
 
   color: #ffffff;
 
-  font-size: ${props => props.cellSize / 2}px;
+  font-size: ${({ cellSize }) => cellSize / 2}px;
   font-weight: bold;
   text-align: center;
 
@@ -77,7 +66,7 @@ const RemoveButtons = styled(Button)`
 
   background-color: #b20000;
 
-  line-height: ${props => props.cellSize - props.cellSize / 5}px;
+  line-height: ${({ cellSize }) => cellSize - cellSize / 5}px;
 `;
 
 const AddRowButton = styled(AddButtons)`
@@ -91,22 +80,24 @@ const AddColumnButton = styled(AddButtons)`
 `;
 
 const RemoveRowButton = styled(RemoveButtons)`
-  visibility: ${props =>
-    props.buttonsVisible === true ? "visible" : "hidden"};
-  opacity: ${props => (props.buttonsVisible === true ? 1 : 0)};
+  visibility: ${({ buttonsVisible }) =>
+    buttonsVisible === true ? "visible" : "hidden"};
+  opacity: ${({ buttonsVisible }) => (buttonsVisible === true ? 1 : 0)};
 
-  top: ${props => props.buttonsPosition.top}px;
+  top: ${({ cellSize, currentRowIndex }) =>
+    cellSize * currentRowIndex + 2 * currentRowIndex}px;
   right: 100%;
   margin: 3px 2px;
 `;
 
 const RemoveColumnButton = styled(RemoveButtons)`
-  visibility: ${props =>
-    props.buttonsVisible === true ? "visible" : "hidden"};
-  opacity: ${props => (props.buttonsVisible === true ? 1 : 0)};
+  visibility: ${({ buttonsVisible }) =>
+    buttonsVisible === true ? "visible" : "hidden"};
+  opacity: ${({ buttonsVisible }) => (buttonsVisible === true ? 1 : 0)};
 
   bottom: 100%;
-  left: ${props => props.buttonsPosition.left}px;
+  left: ${({ cellSize, currentCellIndex }) =>
+    cellSize * currentCellIndex + 2 * currentCellIndex}px;
   margin: 2px 3px;
 `;
 
@@ -118,15 +109,8 @@ export default class Square extends React.Component {
       rows: [],
       columns: [],
 
-      containerPosition: {
-        left: `${this.props.cellSize * 2}px`,
-        top: `${this.props.cellSize * 2}px`
-      },
-
-      buttonsPosition: {
-        left: 0,
-        top: 0
-      },
+      currentCellIndex: 0,
+      currentRowIndex: 0,
 
       buttonsVisible: false,
       removeRowButtonDisplay: true,
@@ -139,17 +123,14 @@ export default class Square extends React.Component {
 
     this.uniqueKey = 0;
 
-    this.currentCellIndex = 0;
-    this.currentRowIndex = 0;
-
     this.offsetX = 0;
     this.offsetY = 0;
-
-    this.onDrag = this.throttle(this.onDrag, 20);
   }
 
   componentDidMount() {
     this.createSquare();
+    document.addEventListener("mousemove", event => this.onDragging(event));
+    document.addEventListener("mouseup", () => this.onDragEnd());
   }
 
   createSquare = () => {
@@ -211,60 +192,29 @@ export default class Square extends React.Component {
     });
   };
 
-  onDrag = ({ pageX, pageY }) => {
+  onDragging = ({ pageX, pageY }) => {
     const { dragging } = this.state;
 
     if (dragging) {
       this.containerRef.current.style.left = pageX - this.offsetX + "px";
       this.containerRef.current.style.top = pageY - this.offsetY + "px";
+      this.containerRef.current.style.margin = 0;
     }
-  };
-
-  onDragging = event => {
-    event.persist();
-    this.onDrag(event);
   };
 
   onDragEnd = () => {
-    const container = this.containerRef.current;
-    const containerStyle = container.style;
-
-    const cloneContainerPosition = { ...this.state.containerPosition };
-    cloneContainerPosition.left = containerStyle.left;
-    cloneContainerPosition.top = containerStyle.top;
-
-    if (containerStyle.left && containerStyle.top) {
-      this.setState({
-        containerPosition: cloneContainerPosition
-      });
-      container.removeAttribute("style");
-    }
-
     this.setState({
       dragging: false
     });
   };
 
   movingButtons = ({ target }) => {
-    const { cellSize } = this.props;
-    const { buttonsPosition } = this.state;
-    const cloneButtonsPosition = { ...buttonsPosition };
-
     if (target.tagName === "TD") {
-      /* In this formula "2" - the padding of each cell, for the correct movement
-    of the button should be considered when calculating */
-      this.currentCellIndex = target.cellIndex;
-      this.currentRowIndex = target.parentNode.rowIndex;
-
-      cloneButtonsPosition.left =
-        cellSize * this.currentCellIndex + 2 * this.currentCellIndex;
-      cloneButtonsPosition.top =
-        cellSize * this.currentRowIndex + 2 * this.currentRowIndex;
+      this.setState({
+        currentCellIndex: target.cellIndex,
+        currentRowIndex: target.parentNode.rowIndex
+      });
     }
-
-    this.setState({
-      buttonsPosition: cloneButtonsPosition
-    });
   };
 
   createColumn = () => {
@@ -286,27 +236,19 @@ export default class Square extends React.Component {
   };
 
   deleteColumn = () => {
-    const { cellSize } = this.props;
-    const { rows, columns, buttonsPosition } = this.state;
-    const cloneButtonsPosition = { ...buttonsPosition };
+    const { currentCellIndex, columns } = this.state;
+    const lastCellIndex = columns.length - 1;
 
-    const cloneRows = [...rows];
-    const cloneColumns = [...columns];
-
-    const columnsLength = cloneColumns.length;
-    const lastCellIndex = columnsLength - 1;
-
-    if (columnsLength > 1) {
-      cloneColumns.splice(this.currentCellIndex, 1);
+    if (columns.length > 1) {
+      this.setState({
+        columns: columns.filter((column, index) => index !== currentCellIndex)
+      });
     }
 
-    if (this.currentCellIndex === lastCellIndex) {
-      /* In this formula "2" - the padding of each cell, for the correct movement
-      of the button should be considered when calculating */
-      cloneButtonsPosition.left =
-        cellSize * (this.currentCellIndex - 1) + 2 * this.currentCellIndex;
-
-      this.currentCellIndex--;
+    if (currentCellIndex === lastCellIndex) {
+      this.setState(prevState => ({
+        currentCellIndex: prevState.currentCellIndex - 1
+      }));
     }
 
     if (lastCellIndex <= 1) {
@@ -314,35 +256,22 @@ export default class Square extends React.Component {
         removeColumnButtonDisplay: false
       });
     }
-
-    this.setState({
-      rows: cloneRows,
-      columns: cloneColumns,
-      buttonsPosition: cloneButtonsPosition
-    });
   };
 
   deleteRow = () => {
-    const { cellSize } = this.props;
-    const { rows, buttonsPosition } = this.state;
-    const cloneButtonsPosition = { ...buttonsPosition };
+    const { currentRowIndex, rows } = this.state;
+    const lastRowIndex = rows.length - 1;
 
-    const cloneRows = [...rows];
-
-    const rowsLength = cloneRows.length;
-    const lastRowIndex = rowsLength - 1;
-
-    if (rowsLength > 1) {
-      cloneRows.splice(this.currentRowIndex, 1);
+    if (rows.length > 1) {
+      this.setState({
+        rows: rows.filter((row, index) => index !== currentRowIndex)
+      });
     }
 
-    if (this.currentRowIndex === lastRowIndex) {
-      /* In this formula "2" - the padding of each cell, for the correct movement
-    of the button should be considered when calculating */
-      cloneButtonsPosition.top =
-        cellSize * (this.currentRowIndex - 1) + 2 * this.currentRowIndex;
-
-      this.currentRowIndex--;
+    if (currentRowIndex === lastRowIndex) {
+      this.setState(prevState => ({
+        currentRowIndex: prevState.currentRowIndex - 1
+      }));
     }
 
     if (lastRowIndex <= 1) {
@@ -350,11 +279,6 @@ export default class Square extends React.Component {
         removeRowButtonDisplay: false
       });
     }
-
-    this.setState({
-      rows: cloneRows,
-      buttonsPosition: cloneButtonsPosition
-    });
   };
 
   showButtons = () => {
@@ -390,75 +314,70 @@ export default class Square extends React.Component {
       buttonsVisible,
       removeRowButtonDisplay,
       removeColumnButtonDisplay,
-      buttonsPosition,
-      containerPosition
+      currentRowIndex,
+      currentCellIndex
     } = this.state;
 
     return (
-      <Wrapper onMouseMove={this.onDragging} onMouseUp={this.onDragEnd}>
-        <Container
-          ref={this.containerRef}
-          containerPosition={containerPosition}
+      <Container
+        ref={this.containerRef}
+        currentRowIndex={currentRowIndex}
+        currentCellIndex={currentCellIndex}
+        cellSize={cellSize}
+        onMouseOver={this.movingButtons}
+      >
+        <div onMouseEnter={this.showButtons} onMouseLeave={this.hideButtons}>
+          <Table onMouseDown={this.onDragStart} onDragStart={() => false}>
+            <tbody>
+              {rows.map(row => (
+                <tr key={`row-${row}`} id="row">
+                  {columns.map(cell => (
+                    <Cell key={`cell-${cell}`} id="cell" cellSize={cellSize} />
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+
+          {removeRowButtonDisplay && (
+            <RemoveRowButton
+              id="remove-row-button"
+              onClick={this.deleteRow}
+              cellSize={cellSize}
+              currentRowIndex={currentRowIndex}
+              buttonsVisible={buttonsVisible}
+            >
+              -
+            </RemoveRowButton>
+          )}
+
+          {removeColumnButtonDisplay && (
+            <RemoveColumnButton
+              id="remove-column-button"
+              onClick={this.deleteColumn}
+              cellSize={cellSize}
+              currentCellIndex={currentCellIndex}
+              buttonsVisible={buttonsVisible}
+            >
+              -
+            </RemoveColumnButton>
+          )}
+        </div>
+        <AddRowButton
+          id="add-row-button"
           cellSize={cellSize}
-          onMouseOver={this.movingButtons}
+          onClick={this.createRow}
         >
-          <div onMouseEnter={this.showButtons} onMouseLeave={this.hideButtons}>
-            <Table onMouseDown={this.onDragStart} onDragStart={() => false}>
-              <tbody>
-                {rows.map(row => (
-                  <tr key={`row-${row}`} id="row">
-                    {columns.map(cell => (
-                      <Cell
-                        key={`cell-${cell}`}
-                        id="cell"
-                        cellSize={cellSize}
-                      />
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-
-            {removeRowButtonDisplay && (
-              <RemoveRowButton
-                id="remove-row-button"
-                onClick={this.deleteRow}
-                cellSize={cellSize}
-                buttonsVisible={buttonsVisible}
-                buttonsPosition={buttonsPosition}
-              >
-                -
-              </RemoveRowButton>
-            )}
-
-            {removeColumnButtonDisplay && (
-              <RemoveColumnButton
-                id="remove-column-button"
-                onClick={this.deleteColumn}
-                cellSize={cellSize}
-                buttonsVisible={buttonsVisible}
-                buttonsPosition={buttonsPosition}
-              >
-                -
-              </RemoveColumnButton>
-            )}
-          </div>
-          <AddRowButton
-            id="add-row-button"
-            cellSize={cellSize}
-            onClick={this.createRow}
-          >
-            +
-          </AddRowButton>
-          <AddColumnButton
-            id="add-column-button"
-            cellSize={cellSize}
-            onClick={this.createColumn}
-          >
-            +
-          </AddColumnButton>
-        </Container>
-      </Wrapper>
+          +
+        </AddRowButton>
+        <AddColumnButton
+          id="add-column-button"
+          cellSize={cellSize}
+          onClick={this.createColumn}
+        >
+          +
+        </AddColumnButton>
+      </Container>
     );
   }
 }
